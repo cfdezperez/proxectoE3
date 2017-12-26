dc77994ff537d42695b2e0f717bb14cbf56f91c0
celia <celia.f.194@gmail.com>
2017-12-26 22:21:26 +0100

    noveno commit

--- src/elementos/Grupo.java
+++ src/elementos/Grupo.java
@@ -5,7 +5,14 @@
  */
 package elementos;
 
+import excepciones.FueraDeMapaException;
+import excepciones.NoRecolectableException;
+import excepciones.ParametroIncorrectoException;
+import excepciones.PersonajeLlenoException;
+import excepciones.RecursosException;
 import interfazUsuario.Juego;
+import java.util.Iterator;
+import java.util.Map;
 import vista.Celda;
 
 /**
@@ -14,11 +21,161 @@ import vista.Celda;
  */
 public class Grupo extends Personaje {
 
+    private int[] capRecoleccion = new int[4]; //0 capacidad total, 1 capacidad madera, 
+    //2 capacidad comida, 3 capacidad piedra
+    private final int capRecoleccionInicial;
     private static int[] numeroGrupos = new int[Civilizacion.getNumDeCivilizaciones()];
 
-    public Grupo(Celda c, Civilizacion civil, int salud, int armadura, int ataque, int capacidad) {
-        super(c, civil, salud, armadura, ataque, capacidad, true, Juego.TGRUPO);
+    public Grupo(Celda c, Civilizacion civil, int salud, int armadura, int ataque, int capacidad) throws ParametroIncorrectoException {
+        super(c, civil, salud, armadura, ataque, true, Juego.TGRUPO);
         numeroGrupos[civil.getIdCivilizacion()]++;
         setNombre("Grupo-" + numeroGrupos[civil.getIdCivilizacion()]);
+        this.capRecoleccion[0] = capacidad < 0 ? 0 : capacidad;
+        this.capRecoleccionInicial = this.capRecoleccion[0];
+    }
+    
+        /**
+     * Capacidad recoleccion total del personaje
+     *
+     * @return int capacidad recolección total
+     */
+    public int getCapRecoleccion() {
+        return this.capRecoleccion[0];
+    }
+
+    /**
+     * Capacidad de recolección para la madera, cantidad de madera que tiene
+     *
+     * @return int capacidad madera
+     */
+    public int getMadera() {
+        return this.capRecoleccion[Recurso.TRMADERA];
+    }
+
+    /**
+     * Capacidad de recolección para la comida, cantidad de comida que tiene
+     *
+     * @return int capacidad comida
+     */
+    public int getComida() {
+        return this.capRecoleccion[Recurso.TRCOMIDA];
+    }
+
+    /**
+     * Capacidad de recolección para la piedra, cantidad de piedra que tiene
+     *
+     * @return int capacidad piedra
+     */
+    public int getPiedra() {
+        return this.capRecoleccion[Recurso.TRPIEDRA];
+    }
+    
+        public int getCRInicial() {
+        return this.capRecoleccionInicial;
+    }
+
+    public int getRecursoTipo(int tipo) {
+        switch (tipo) {
+            case Recurso.TRMADERA:
+                return capRecoleccion[Recurso.TRMADERA];
+            case Recurso.TRCOMIDA:
+                return capRecoleccion[Recurso.TRCOMIDA];
+            case Recurso.TRPIEDRA:
+                return capRecoleccion[Recurso.TRPIEDRA];
+            default:
+                System.out.println("El recurso no existe");
+                return 0;
+        }
+    }
+    
+    
+        /**
+     *
+     * @param cap
+     */
+    public void setCapRecoleccion(int cap) {
+        if (cap < 0) {
+            this.capRecoleccion[0] = 0;
+        } else {
+            this.capRecoleccion[0] = cap;
+        }
+    }
+
+    public void setCapRecoleccionTipo(int cap, int tipo) {
+        if (cap < 0) {
+            cap = 0;
+        }
+        this.capRecoleccion[tipo] = cap;
+    }
+    
+        public void setMadera(int a) {
+        this.capRecoleccion[Recurso.TRMADERA] = a;
+    }
+
+    public void setComida(int a) {
+        this.capRecoleccion[Recurso.TRCOMIDA] = a;
+    }
+
+    public void setPiedra(int a) {
+        this.capRecoleccion[Recurso.TRPIEDRA] = a;
+    }
+    
+    /**
+     *
+     * @param direccion
+     * @throws excepciones.FueraDeMapaException
+     * @throws excepciones.ParametroIncorrectoException
+     * @throws excepciones.NoRecolectableException
+     * @throws excepciones.PersonajeLlenoException
+     */
+    public void recolectar(String direccion) throws RecursosException, PersonajeLlenoException, FueraDeMapaException, ParametroIncorrectoException, NoRecolectableException {
+        Celda actual = this.getCelda();
+        Celda vecina = actual.getMapa().obtenerCeldaVecina(actual, direccion);
+
+        if (vecina.getContRecurso() == null) {
+            throw new NoRecolectableException("La celda no contiene un contenedor de recursos");
+        }
+
+        ContRecurso cr = vecina.getContRecurso();
+        //Solo va a haber un elemento en la celd que va a ser un contenedor de recurso
+        //se restan de la capacidad del contenedor de recursos la capacidad 
+        // que tiene el personaje para recolectar
+        int disponible = cr.getRecurso().getCapacidad(); //Capacidad disponible en ese momento
+        int recolectado;
+        if (this.getCapRecoleccion() == 0) {
+            throw new PersonajeLlenoException("El personaje agotó su capacidad de recolección");
+        } else if (disponible > this.getCapRecoleccion()) {
+            recolectado = this.getCapRecoleccion();
+            cr.getRecurso().setCapacidad(disponible - recolectado); //cambia capacidad para recolectar en funcion de la recolectado
+            this.capRecoleccion[0] = 0;
+            this.capRecoleccion[cr.getRecurso().getTipo()] += recolectado;
+            System.out.println("El paisano ha conseguido " + recolectado + " unidades de " + cr.getRecurso().getNombre());
+        } else if (this.getCapRecoleccion() >= disponible) {
+            recolectado = disponible;
+            this.capRecoleccion[0] -= recolectado;
+            this.capRecoleccion[cr.getRecurso().getTipo()] += recolectado;
+
+            // Convertimos la celda en pradera
+            vecina.restartCelda();
+
+            System.out.println("Has conseguido " + recolectado + " unidades de " + cr.getRecurso().getNombre());
+        } else { //Si disponible, que 
+            throw new RecursosException("Error al recolectar");
+        }
+    }
+    
+        private void compruebaSoldadoEnGrupo() {
+        Iterator it = this.getPersonajes().entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry a = (Map.Entry) it.next();
+            if (((Personaje) a.getValue()).getTipo() == Mapa.TSOLDADO) {
+                this.capEdificacion = false;
+                this.capRecoleccion[0] = 0;
+                this.capRecoleccion[Recurso.TRMADERA] = 0;
+                this.capRecoleccion[Recurso.TRCOMIDA] = 0;
+                this.capRecoleccion[Recurso.TRPIEDRA] = 0;
+                break;
+            }
+        }
     }
 }
