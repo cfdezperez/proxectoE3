f6e686dd0e689f76bdee1fe6822ca8118c3eadde
celia <celia.f.194@gmail.com>
2017-12-27 20:45:20 +0100

    trece commit
    Comentario en obtenerCelda()

--- src/interfazUsuario/Proyecto_final.java
+++ src/interfazUsuario/Proyecto_final.java
@@ -40,7 +40,8 @@ public class Proyecto_final {
         try {
             Juego j = Menu.cargarFicheros("src/ficherosEvaluacion");
             j.getMapa().imprimir();
-            System.out.println(j.getMapa().obtenerCelda(0, 1).mirar());
+            //Se introduce primero la columna, que será x y luego la fila que será y
+            System.out.println(j.getMapa().obtenerCelda(1, 2).mirar());
         } catch (ParametroIncorrectoException ex) {
             Logger.getLogger(Proyecto_final.class.getName()).log(Level.SEVERE, null, ex);
         } catch (FueraDeMapaException ex) {
