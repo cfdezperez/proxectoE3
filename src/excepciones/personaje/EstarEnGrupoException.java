4d864e2aa25d8be5aad4f2520c493a7f345c5daa
Tomás Fernández Pena <tf.pena@usc.es>
2017-12-28 22:26:46 +0100

    Nuevas excepciones para grupos

---
+++ src/excepciones/personaje/EstarEnGrupoException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones.personaje;
+
+/**
+ *
+ * @author tomas
+ */
+public class EstarEnGrupoException extends GrupoException {
+    public EstarEnGrupoException(String m) {
+        super(m);
+    }
+}
---
+++ src/excepciones/personaje/GrupoException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones.personaje;
+
+/**
+ *
+ * @author tomas
+ */
+public class GrupoException  extends PersonajeException {
+    public GrupoException(String m) {
+        super(m);
+    }
+}
