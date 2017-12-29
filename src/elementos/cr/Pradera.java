6497837bcfd34e536667194a564d20796863f0b1
maria <maria@maria-XPS-13-9360>
2017-12-29 19:10:20 +0100

    borrar una cosa

--- src/elementos/cr/Pradera.java
+++ src/elementos/cr/Pradera.java
@@ -32,29 +32,3 @@ public class Pradera extends ContRecurso{
         throw new NoProcesableException("Las praderas no se procesan");
     }    
 }
-/*/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- 
-package elementos.cr;
-
-import elementos.ContRecurso;
-import elementos.Recurso;
-import excepciones.celda.CeldaOcupadaException;
-import interfazUsuario.Juego;
-import vista.Celda;
-
-
-public abstract class Pradera extends ContRecurso{
-    
-    public Pradera() {
-        super(new Recurso(Juego.TPRADERA, 0));
-        setTransitable(true);
-    }
-    
-    @Override
-    public String toString() {
-        return("\n\tContenedor de recursos de tipo Pradera, no productiva");
-    } 
-}*/
